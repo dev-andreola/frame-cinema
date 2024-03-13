64a5a8a75c38f4d79d2f976a01941eac3d85d550
vtAndreola <dev.andreola@gmail.com>
2024-03-13 14:23:50 -0300

    feat: Add movie page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,11 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
   return (
     <div className="flex dark:bg-gray-600 bg-sky-100 p-4 lg:text-lg justify-center gap-6">
-      <NavbarItem title="Popular" param="fetchTrending" />
+      <NavbarItem
+        className="underline underline-offset-8 decoration-4 decoration-sky-500 rounded-lg"
+        title="Popular"
+        param="fetchTrending"
+      />
       <NavbarItem title="Melhor Classificação" param="fetchTopRated" />
     </div>
   );
