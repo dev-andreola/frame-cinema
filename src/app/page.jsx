0a678917880e973bd9bd14b314ba1593f14863f0
vtAndreola <dev.andreola@gmail.com>
2024-03-13 10:10:17 -0300

    feat: Fetch data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export default function Home() {
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=pt-BR&page=1`
+  );
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+  console.log(results);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
