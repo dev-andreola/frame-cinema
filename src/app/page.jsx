311c5c7f5e4bd9a332ca161ccfae967fd142cafa
vtAndreola <dev.andreola@gmail.com>
2024-03-13 11:07:43 -0300

    feat: Add loading effect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,11 +6,14 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
+
   const response = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=pt-BR&page=1`
+    }?api_key=${API_KEY}&language=pt-BR&page=1`,
+    { next: { revalidate: 10000 } }
   );
+
   const data = await response.json();
   if (!response.ok) {
     throw new Error("Failed to fetch data");
