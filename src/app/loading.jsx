7fe5252bdc3c6f19f0b32c68b66f8f1742035ce2
vtAndreola <dev.andreola@gmail.com>
2024-03-15 18:14:38 -0300

    fix: Loading next image

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <img className="h-52" src="spinner.svg" alt="Loading" />
+      <Image src="/spinner.svg" alt="Loading" width={130} height={130} />
     </div>
   );
 }
