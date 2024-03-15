7bdda4417f0e0b5f700ddaef294975090f2589e2
vtAndreola <dev.andreola@gmail.com>
2024-03-15 18:15:06 -0300

    fix: Add suspense tag

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,26 +1,33 @@
 "use client";
 
-import React from "react";
+import { Suspense } from "react";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
-import Link from "next/link";
+// Componente NavbarItem agora está dentro do componente NavbarItemWrapper
+export default function NavbarItemWrapper({ title, param }) {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <NavbarItem title={title} param={param} />
+    </Suspense>
+  );
+}
 
-export default function NavbarItem({ title, param }) {
+// NavbarItem continua o mesmo
+function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
 
   return (
-    <div>
-      <Link
-        className={`hover:text-sky-500 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-sky-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Link
+      className={`hover:text-sky-500 font-semibold ${
+        genre === param
+          ? "underline underline-offset-8 decoration-4 decoration-sky-500 rounded-lg"
+          : ""
+      }`}
+      href={`/?genre=${param}`}
+    >
+      {title}
+    </Link>
   );
 }
